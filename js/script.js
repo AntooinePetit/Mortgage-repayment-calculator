4c3dcfccb1e402376b101cbf11b9a0e61149d143
unknown <antooine.petit@gmail.com>
2025-03-27 12:16:51 +0100

    style: improve form styles and add results section
    - Updated form styles and spacing
    - Added input focus and error styles
    - Created new styles for results section
    - Improved responsiveness and layout
    - Added new classes for better styling

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+<h2>Your results</h2>
+<p>Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p>
+<div>
+   <p>Your monthly repayments</p>
+   <p id="result-repayments">£1,797.74</p>
+   <hr>
+   <p>Total you'll repay over the term</p>
+   <p id="result-total">£539,322.94</p>
+</div>
+*/
